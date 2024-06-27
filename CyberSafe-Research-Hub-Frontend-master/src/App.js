1329de8ffbb29282331f5cb01cf58c459eda140d
sabikunnahar16 <sabikunnahar789789@gmail.com>
2024-06-27 23:46:06 +0600

    Sabikunnahar16

---
+++ CyberSafe-Research-Hub-Frontend-master/src/App.js
@@ -0,0 +1,52 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+      <>
+  <nav>
+  <div class="logo">
+                <img src="shield-keyhole-blue-cyber-security-logo-Graphics-20359585-1.jpg" alt="img">
+    
+    
+        <ul id="navbar">
+            <li><a classname="active" 
+            href="index.html">Home</a></li>
+            <li><a href="index.html">Blog</a></li>
+            <li><a href="index.html">Members</a></li>
+            <li><a href="index.html">About</a></li>
+            <li><a href="index.html">Contact</a></li>
+            <li><a href="index.html">SignUp</a></li>
+        </ul>
+        </div>
+    </nav>
+    <div className="background">
+        <h1>Welcome to CyberSafe Research-Hub</h1>
+      </div>
+      <div class="separation"></div>
+      <footer className="footer">
+        
+      <div class="credit">
+                <span><img src="https://www.vlrtraining.in/wp-content/uploads/2020/10/logo-cyber-security.png" alt="img"></span>
+                <span>&copy 2024 CyberSafe Research-Hub LLC All rights reserved.</span>
+            </div>
+            <div class="faq">
+                <div class="faq1">
+                    <a href="FAQ">FAQ</a>
+                    <a href="Privacy">Privacy</a>
+                    <a href="Help Center">Help Center</a>
+                </div>
+                <div class="faq1">
+                    <a href="Contact">Contact</a>
+                    
+                    <a href="Terms of use">Terms of use</a>
+                </div>
+            </div>
+      </footer>
+    </>
+      
+  );
+}
+
+export default App;
